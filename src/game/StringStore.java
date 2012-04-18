2141669e58532549e52217d62673469dbae4adb0
Alexandre GILLE <alexandre.teva.gille@gmail.com>
2012-04-18 16:32:38 +0200

    - StringStore.java

---
+++ src/game/StringStore.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Classe static non-instanciable permettant de stocker toutes les chaînes de caractères du jeux.
+ * @author Alex
+ *
+ */
+public final class StringStore {
+	
+	public static final String APP_TITLE = "StartRacing";
+	
+}

6691f15033799e7b003124abf19ac5f3cc7dd4f2
Arnaud TANGUY <arn.tanguy@gmail.com>
2012-05-29 16:42:10 +0200

    Fix stopping VoiceRender

--- src/audio/VoiceRender.java
+++ src/audio/VoiceRender.java
@@ -4,10 +4,11 @@ import com.jme3.audio.AudioNode;
 import com.jme3.scene.Node;
 
 /**
- * Play the sounds for the vocal help.
- * It plays only one sound at a time, preventing sounds to overlap
+ * Play the sounds for the vocal help. It plays only one sound at a time,
+ * preventing sounds to overlap
+ * 
  * @author TANGUY Arnaud
- *
+ * 
  */
 public class VoiceRender extends AudioRender<String> {
 
@@ -17,39 +18,46 @@ public class VoiceRender extends AudioRender<String> {
 		super(rootNode, soundStore);
 		playedNode = null;
 	}
+
 	/**
-	 * Plays the voice corresponding to the given id, only if it is not already playing.
-	 * If it is a different id than the one being played, stop the current playing, and play the new one.
+	 * Plays the voice corresponding to the given id, only if it is not already
+	 * playing. If it is a different id than the one being played, stop the
+	 * current playing, and play the new one.
+	 * 
 	 * @param sound
-	 * 		The string id of the sound
-	 * @throws Exception 
+	 *            The string id of the sound
+	 * @throws Exception
 	 */
 	public void playVoice(String sound) {
-//		System.out.println("play " + sound);
+		// System.out.println("play " + sound);
 		if (playedNode == null) {
 			playedNode = soundNodes.get(sound);
 			play(sound, false);
-//			System.out.println(playedNode.getStatus());
+			// System.out.println(playedNode.getStatus());
 		} else {
 			if (!playedNode.getName().equals(sound)) {
-				if(playedNode.getStatus() == AudioNode.Status.Playing) {
-//					System.out.println("Sound "+playedNode.getName()+" is playing, stopping it...");
+				if (playedNode.getStatus() == AudioNode.Status.Playing) {
+					// System.out.println("Sound "+playedNode.getName()+" is playing, stopping it...");
 					stopVoice();
 				}
-//				System.out.println("Playing " + sound);
+				// System.out.println("Playing " + sound);
 				playedNode = soundNodes.get(sound);
 				play(sound, false);
 			} else {
-//				System.out.println("Same name, do nothing");
+				// System.out.println("Same name, do nothing");
 			}
 		}
 	}
+
 	/**
 	 * Stops the current playing track
 	 */
 	public void stopVoice() {
-		System.out.println("VoiceRender :: Stop voice "+playedNode.getName());
-		playedNode.stop();
+		if (playedNode != null) {
+			System.out.println("VoiceRender :: Stop voice "
+					+ playedNode.getName());
+			playedNode.stop();
+		}
 	}
 
 	/**
